5ba1a501b16677bd8af1851e23cf92c079c55234
suvadeep91 <omniel99@gmail.com>
2014-11-22 09:41:03 +0530

    Adding  definition of first funtion datainut method.

--- program.cpp
+++ program.cpp
@@ -6,5 +6,14 @@ void finaloutput();
 void main()
 {
   float ca1,ca2,ca3;
+  void datainput()
+  {
+	  cout << "Enter CA1 Marks:";
+	  cin >> ca1;
+	  cout << "Enter CA1 Marks:";
+	  cin >> ca2;
+	  cout << "Enter CA3 Marks:";
+	  cin >> ca3;
+  }
   getch();
 }
