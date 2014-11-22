daef52399ffe0640acae26b01886d27fafab80f1
suvadeep91 <omniel99@gmail.com>
2014-11-22 10:02:22 +0530

    Added definition of method best_two()

--- program.cpp
+++ program.cpp
@@ -5,7 +5,7 @@ void best_two();
 void finaloutput();
 void main()
 {
-  float ca1,ca2,ca3;
+  float ca1,ca2,ca3,sum=0;
   void datainput()
   {
 	  cout << "Enter CA1 Marks:";
@@ -16,5 +16,39 @@ void main()
 	  cin >> ca3;
   }
   //Methods is correct and takes all the requirements for CA calculation
+  void best_two()
+  {
+	  if (ca1 >= ca2)
+	  {
+		  cout << "CA1 is selected";
+		  sum = sum + ca1;
+		  if (ca3 >= ca2)
+		  {
+			  cout << "CA3 is selected";
+			  sum = sum + ca3;
+		  }
+		  else
+		  {
+			  cout << "CA2 is selected";
+			  sum = sum + ca2;
+		  }
+
+	  }
+	  else
+	  {
+		  cout << "CA2 is selected";
+		  sum = sum + ca2;
+		  if (ca3 >= ca1)
+		  {
+			  cout << "CA3 is selected";
+			  sum = sum + ca3;
+		  }
+		  else
+		  {
+			  cout << "CA1 is selected";
+			  sum = sum + ca1;
+		  }
+	  }
+  }
   getch();
 }
